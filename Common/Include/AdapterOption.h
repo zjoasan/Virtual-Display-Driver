6bfc3735cedafa85c68adc2ff882515b070132f1
Joakim Sandström <joakim.s@webbkontakt.net>
2025-11-06 20:49:10 +0100

    Removed most fail points
    Now it will take "Friendlyname, 3" for PCI-bus 3, Device 0, Function 0. If Frienlyname alone it will match with the first, else as allways, bestGpu(most vram) selected.
    
    Now it compiles again, I had to up the C++ version to 17 in order to get "optional" working. There are probably tricks for it, but i don't have time or need to put effort into this.
    DevID and iDX stuff remove due to Kernel/Usermode not playing nicely.
    
    Will see if i up a temporaary  ".snl and/or .vxproj.user" for simplicity to build

--- Common/Include/AdapterOption.h
+++ Common/Include/AdapterOption.h
@@ -1,201 +1,119 @@
 #pragma once
 
-#include <wrl/client.h>     // For ComPtr
-#include <dxgi.h>           // For IDXGIAdapter, IDXGIFactory1
-#include <algorithm>        // For sort
-#include <d3dkmt.h>         // For D3DKMT functions
-#include <setupapi.h>       // For SetupAPI
-#include <devpkey.h>        // For DEVPKEY
-#include <cfgmgr32.h>       // For CM_ functions
-#include <fstream>          // For file I/O
+#include <wrl/client.h>
+#include <dxgi1_6.h>
+#include <setupapi.h>
+#include <devguid.h>
 #include <string>
+#include <vector>
+#include <optional>
+#include <algorithm>
+#include <fstream>
 
 using namespace std;
 using namespace Microsoft::WRL;
 
-// Structure to hold GPU information
 struct GPUInfo {
-    wstring name;                // GPU friendly name (from DXGI_ADAPTER_DESC)
-    ComPtr<IDXGIAdapter> adapter;// COM pointer to the adapter
-    DXGI_ADAPTER_DESC desc;      // Adapter description
-    wstring deviceId;            // Device Instance ID
-    wstring slotInfo;            // PCI slot information (e.g., "PCI Slot 2")
+    wstring name;
+    ComPtr<IDXGIAdapter> adapter;
+    DXGI_ADAPTER_DESC desc;
 };
 
-// Sort function for GPUs by dedicated video memory
 bool CompareGPUs(const GPUInfo& a, const GPUInfo& b) {
     return a.desc.DedicatedVideoMemory > b.desc.DedicatedVideoMemory;
 }
 
-// Helper function to get Device Instance ID and PCI slot info from LUID
-struct AdapterDeviceInfo {
-    wstring deviceId;
-    wstring slotInfo;
-    LUID luid;
-};
-
-vector<AdapterDeviceInfo> GetAdapterDeviceInfos() {
-    vector<AdapterDeviceInfo> adapterInfos;
-
-    // Enumerate display adapter interfaces
-    HDEVINFO devInfo = SetupDiGetClassDevsW(&GUID_DEVINTERFACE_DISPLAY_ADAPTER, nullptr, nullptr, DIGCF_DEVICEINTERFACE | DIGCF_PRESENT);
-    if (devInfo == INVALID_HANDLE_VALUE) {
-        return adapterInfos;
-    }
-
-    SP_DEVICE_INTERFACE_DATA interfaceData = { sizeof(SP_DEVICE_INTERFACE_DATA) };
-    for (DWORD i = 0; SetupDiEnumDeviceInterfaces(devInfo, nullptr, &GUID_DEVINTERFACE_DISPLAY_ADAPTER, i, &interfaceData); ++i) {
-        // Get device interface path
-        DWORD requiredSize = 0;
-        SetupDiGetDeviceInterfaceDetailW(devInfo, &interfaceData, nullptr, 0, &requiredSize, nullptr);
-        vector<WCHAR> buffer(requiredSize / sizeof(WCHAR));
-        PSP_DEVICE_INTERFACE_DETAIL_DATA_W detail = reinterpret_cast<PSP_DEVICE_INTERFACE_DETAIL_DATA_W>(buffer.data());
-        detail->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_W);
-        if (!SetupDiGetDeviceInterfaceDetailW(devInfo, &interfaceData, detail, requiredSize, nullptr, nullptr)) {
-            continue;
-        }
-
-        // Get LUID
-        D3DKMT_OPENADAPTERFROMDEVICENAME openAdapter = {};
-        openAdapter.pDeviceName = detail->DevicePath;
-        NTSTATUS status = D3DKMTOpenAdapterFromDeviceName(&openAdapter);
-        if (!NT_SUCCESS(status)) {
-            continue;
-        }
-
-        // Get Device Instance ID
-        ULONG propSize = 0;
-        DEVPROPTYPE propType;
-        CM_Get_Device_Interface_PropertyW(detail->DevicePath, &DEVPKEY_Device_InstanceId, &propType, nullptr, &propSize, 0);
-        vector<BYTE> propBuffer(propSize);
-        wstring deviceId;
-        if (CM_Get_Device_Interface_PropertyW(detail->DevicePath, &DEVPKEY_Device_InstanceId, &propType, propBuffer.data(), &propSize, 0) == CR_SUCCESS) {
-            deviceId = reinterpret_cast<PWCHAR>(propBuffer.data());
-        }
-
-        // Get PCI slot info
-        propSize = 0;
-        CM_Get_Device_Interface_PropertyW(detail->DevicePath, &DEVPKEY_Device_LocationInfo, &propType, nullptr, &propSize, 0);
-        propBuffer.resize(propSize);
-        wstring slotInfo;
-        if (CM_Get_Device_Interface_PropertyW(detail->DevicePath, &DEVPKEY_Device_LocationInfo, &propType, propBuffer.data(), &propSize, 0) == CR_SUCCESS) {
-            slotInfo = reinterpret_cast<PWCHAR>(propBuffer.data());
-        }
-
-        adapterInfos.push_back({ deviceId, slotInfo, openAdapter.AdapterLuid });
-
-        D3DKMT_CLOSEADAPTER closeAdapter = { openAdapter.hAdapter };
-        D3DKMTCloseAdapter(&closeAdapter);
-    }
-
-    SetupDiDestroyDeviceInfoList(devInfo);
-    return adapterInfos;
-}
-
-// Get a list of available GPUs with Device Instance ID and PCI slot info
 vector<GPUInfo> getAvailableGPUs() {
     vector<GPUInfo> gpus;
-
     ComPtr<IDXGIFactory1> factory;
-    if (!SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory)))) {
-        return gpus;
-    }
+    if (!SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory)))) return gpus;
 
-    // Get Device Instance IDs and slot info
-    auto deviceInfos = GetAdapterDeviceInfos();
-
-    // Enumerate all adapters (GPUs)
     for (UINT i = 0;; i++) {
         ComPtr<IDXGIAdapter> adapter;
-        if (!SUCCEEDED(factory->EnumAdapters(i, &adapter))) {
-            break;
-        }
+        if (!SUCCEEDED(factory->EnumAdapters(i, &adapter))) break;
 
         DXGI_ADAPTER_DESC desc;
-        if (!SUCCEEDED(adapter->GetDesc(&desc))) {
-            continue;
-        }
+        if (!SUCCEEDED(adapter->GetDesc(&desc))) continue;
 
-        // Find matching Device Instance ID and slot info
-        wstring deviceId, slotInfo;
-        for (const auto& deviceInfo : deviceInfos) {
-            if (RtlEqualLuid(&desc.AdapterLuid, &deviceInfo.luid)) {
-                deviceId = deviceInfo.deviceId;
-                slotInfo = deviceInfo.slotInfo;
-                break;
-            }
-        }
-
-        // Add the adapter information to the list
-        GPUInfo info{ desc.Description, adapter, desc, deviceId, slotInfo };
-        gpus.push_back(info);
+        gpus.push_back({ desc.Description, adapter, desc });
     }
-
     return gpus;
+}
+
+std::optional<LUID> resolveLuidFromNameAndBus(const wstring& name, uint32_t busIndex) {
+    ComPtr<IDXGIFactory1> factory;
+    if (!SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory)))) return std::nullopt;
+
+    for (UINT i = 0;; ++i) {
+        ComPtr<IDXGIAdapter1> adapter;
+        if (factory->EnumAdapters1(i, &adapter) == DXGI_ERROR_NOT_FOUND) break;
+
+        DXGI_ADAPTER_DESC1 desc;
+        adapter->GetDesc1(&desc);
+
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) continue;
+        if (name.length() && wcsstr(desc.Description, name.c_str())) {
+            // Match name, now verify PCI bus index
+            HDEVINFO devInfo = SetupDiGetClassDevsW(&GUID_DEVCLASS_DISPLAY, nullptr, nullptr, DIGCF_PRESENT);
+            if (devInfo == INVALID_HANDLE_VALUE) continue;
+
+            SP_DEVINFO_DATA devData = {};
+            devData.cbSize = sizeof(devData);
+
+            for (DWORD j = 0; SetupDiEnumDeviceInfo(devInfo, j, &devData); ++j) {
+                WCHAR location[256];
+                DWORD type;
+                ULONG size;
+
+                if (!SetupDiGetDeviceRegistryPropertyW(devInfo, &devData, SPDRP_LOCATION_INFORMATION,
+                    &type, (PBYTE)location, sizeof(location), &size)) continue;
+
+                wstring loc = location;
+                size_t pos = loc.find(L"PCI bus ");
+                if (pos == wstring::npos) continue;
+
+                uint32_t parsedBus = wcstoul(loc.substr(pos + 8).c_str(), nullptr, 10);
+                if (parsedBus == busIndex) {
+                    SetupDiDestroyDeviceInfoList(devInfo);
+                    return desc.AdapterLuid;
+                }
+            }
+            SetupDiDestroyDeviceInfoList(devInfo);
+        }
+    }
+    return std::nullopt;
 }
 
 class AdapterOption {
 public:
-    bool hasTargetAdapter{}; // Indicates if a target adapter is selected
-    LUID adapterLuid{};      // Adapter's unique identifier (LUID)
-    wstring target_name{};   // Target adapter name (friendlyname,slot)
-    wstring target_device_id{}; // Device Instance ID of selected adapter
+    bool hasTargetAdapter{};
+    LUID adapterLuid{};
+    wstring target_name{};
 
-    // Select the best GPU based on dedicated video memory
     wstring selectBestGPU() {
         auto gpus = getAvailableGPUs();
-        if (gpus.empty()) {
-            return L""; // Error check for headless/VM
-        }
-
-        // Sort GPUs by dedicated video memory in descending order
+        if (gpus.empty()) return L"";
         sort(gpus.begin(), gpus.end(), CompareGPUs);
-        auto bestGPU = gpus.front(); // Get the GPU with the most memory
-
-        // Return friendlyname,slot format
-        return bestGPU.name + (bestGPU.slotInfo.empty() ? L"" : L"," + bestGPU.slotInfo);
+        return gpus.front().name;
     }
 
-    // Parse friendlyname,pcislot input
-    pair<wstring, wstring> parseTarget(const wstring& input) {
-        auto pos = input.find(L',');
-        if (pos == wstring::npos) {
-            return { input, L"" }; // No slot specified
-        }
-        return { input.substr(0, pos), input.substr(pos + 1) }; // Friendly name, slot
-    }
-
-    // Load friendlyname,slot from a file or select the best GPU
     void load(const wchar_t* path) {
         ifstream ifs{ path };
         if (!ifs.is_open()) {
             target_name = selectBestGPU();
-        }
-        else {
+        } else {
             string line;
             getline(ifs, line);
             target_name.assign(line.begin(), line.end());
         }
-
-        // Find and set the adapter based on the target name and slot
-        if (!findAndSetAdapter(target_name)) {
-            // If the adapter is not found, select the best GPU and retry
-            target_name = selectBestGPU();
-            findAndSetAdapter(target_name);
-        }
+        findAndSetAdapter(target_name);
     }
 
-    // Set the target adapter from a given name and slot (e.g., "NVIDIA GeForce RTX 3080,PCI Slot 2")
     void xmlprovide(const wstring& xtarg) {
         target_name = xtarg;
-        if (!findAndSetAdapter(target_name)) {
-            // If the adapter is not found, select the best GPU and retry
-            target_name = selectBestGPU();
-            findAndSetAdapter(target_name);
-        }
+        findAndSetAdapter(target_name);
     }
 
-    // Apply the adapter settings to the specified adapter
     void apply(const IDDCX_ADAPTER& adapter) {
         if (hasTargetAdapter && IDD_IS_FUNCTION_AVAILABLE(IddCxAdapterSetRenderAdapter)) {
             IDARG_IN_ADAPTERSETRENDERADAPTER arg{};
@@ -205,53 +123,28 @@ public:
     }
 
 private:
-    // Find and set the adapter by its friendly name and optional PCI slot
-    bool findAndSetAdapter(const wstring& target) {
-        auto gpus = getAvailableGPUs();
-        if (gpus.empty()) {
-            hasTargetAdapter = false;
-            return false;
-        }
-
-        auto [targetName, targetSlot] = parseTarget(target);
-
-        // Find all adapters matching the friendly name
-        vector<GPUInfo> matchingGPUs;
-        for (const auto& gpu : gpus) {
-            if (_wcsicmp(gpu.name.c_str(), targetName.c_str()) == 0) {
-                matchingGPUs.push_back(gpu);
-            }
-        }
-
-        if (matchingGPUs.empty()) {
-            hasTargetAdapter = false;
-            return false;
-        }
-
-        // If only one match or no slot specified, use the first match
-        if (matchingGPUs.size() == 1 || targetSlot.empty()) {
-            const auto& gpu = matchingGPUs.front();
-            adapterLuid = gpu.desc.AdapterLuid;
-            target_device_id = gpu.deviceId;
-            hasTargetAdapter = true;
-            return true;
-        }
-
-        // Multiple matches: use PCI slot to disambiguate
-        for (const auto& gpu : matchingGPUs) {
-            if (_wcsicmp(gpu.slotInfo.c_str(), targetSlot.c_str()) == 0) {
-                adapterLuid = gpu.desc.AdapterLuid;
-                target_device_id = gpu.deviceId;
+    bool findAndSetAdapter(const wstring& adapterSpec) {
+        size_t comma = adapterSpec.find(L',');
+        if (comma != wstring::npos) {
+            wstring name = adapterSpec.substr(0, comma);
+            uint32_t bus = wcstoul(adapterSpec.substr(comma + 1).c_str(), nullptr, 10);
+            auto luidOpt = resolveLuidFromNameAndBus(name, bus);
+            if (luidOpt.has_value()) {
+                adapterLuid = luidOpt.value();
                 hasTargetAdapter = true;
                 return true;
             }
+        } else {
+            auto gpus = getAvailableGPUs();
+            for (const auto& gpu : gpus) {
+                if (_wcsicmp(gpu.name.c_str(), adapterSpec.c_str()) == 0) {
+                    adapterLuid = gpu.desc.AdapterLuid;
+                    hasTargetAdapter = true;
+                    return true;
+                }
+            }
         }
-
-        // No slot match found; fall back to first matching GPU
-        const auto& gpu = matchingGPUs.front();
-        adapterLuid = gpu.desc.AdapterLuid;
-        target_device_id = gpu.deviceId;
-        hasTargetAdapter = true;
-        return true;
+        hasTargetAdapter = false;
+        return false;
     }
 };
